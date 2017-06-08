1cc2452bf492d8f1342286e70c4984e49c08831b
George <giorgi.nebieridze.gn@gmail.com>
2017-06-08 11:38:30 -0700

    commit

--- app/src/main/java/com/sitename/android/saeklesiokalendari/Parsers/JsonParser.java
+++ app/src/main/java/com/sitename/android/saeklesiokalendari/Parsers/JsonParser.java
@@ -1,10 +1,5 @@
 package com.sitename.android.saeklesiokalendari.Parsers;
 
-import com.sitename.android.saeklesiokalendari.Internet.GetJsonData;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class JsonParser {
 
 }
